91352c59446dfea9d2773798c1cf0c84470c4403
Ryan Paik <600021343@fjuhsd.org>
2021-02-27 05:14:57 +0000

    Added more test cases.

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Ryan Paik
 //Date -
 
 //add imports as needed
@@ -14,5 +14,12 @@ public class Main
 System.out.println( l.go( Arrays.asList(-99,1,2,3,4,5,6,7,8,9,10,5) ) );
 System.out.println( l.go( Arrays.asList(10,9,8,7,6,5,4,3,2,1,-99) ) );
 System.out.println( l.go( Arrays.asList(10,20,30,40,50,-11818,40,30,20,10) ) );
+System.out.println( l.go( Arrays.asList(9,10,-88,100,-555,1000)));
+System.out.println( l.go( Arrays.asList(10,10,10,11,456)));
+System.out.println( l.go( Arrays.asList(-111,1,2,3,9,11,20,30)));
+System.out.println( l.go( Arrays.asList(9,8,7,6,5,4,3,2,0,-2,9,9)));
+System.out.println( l.go( Arrays.asList(12,15,18,21,23,10,12)));
+System.out.println( l.go( Arrays.asList(250,19,17,15,13,11,10,9,6,3,2,1,-455)));
+System.out.println( l.go( Arrays.asList(9,10,-8,10000,-5000,1000)));
  }
 }
